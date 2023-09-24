c12f7e9feee50159507daeaa6e4e7a0541a7e395
Jty98 <clclcfnfn@naver.com>
2023-09-24 18:33:11 +0900

    Main에 Test 로그아웃버튼 생성

--- TravelMaker/ViewController/MainVC/MainPageViewController.swift
+++ TravelMaker/ViewController/MainVC/MainPageViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainPageViewController: UIViewController {
 
@@ -16,14 +17,35 @@ class MainPageViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnLogout(_ sender: UIButton) {
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+                // 로그아웃 성공한 경우, 이미지를 변경하거나 다른 작업 수행
+                
+                let resultAlert = UIAlertController(title: "로그아웃", message: "로그아웃 되었습니다", preferredStyle: .actionSheet)
+                let onAction = UIAlertAction(title: "OK", style: .default, handler: {ACTION in self.transitionToLogout()})
+                resultAlert.addAction(onAction)
+                self.present(resultAlert, animated: true)
+            } catch {
+                // 로그아웃 실패한 경우, 에러 메시지 출력
+                print(error.localizedDescription)
+            }
+        } else {
+            // 사용자가 로그인하지 않은 경우
+        }
     }
-    */
+    
+    // 로그아웃시 첫화면 이동
+    func transitionToLogout(){
+        let board = UIStoryboard(name: "Main", bundle: nil)
+        guard let nextVC = board.instantiateViewController(withIdentifier: "LoginSB") as? LoginViewController else { return }
+    //        nextVC.userData = data
+        nextVC.modalTransitionStyle = .coverVertical
+        nextVC.modalPresentationStyle = .overFullScreen
+        self.present(nextVC, animated: true)
+    }
 
-}
+   
+
+} // MainPageViewController
