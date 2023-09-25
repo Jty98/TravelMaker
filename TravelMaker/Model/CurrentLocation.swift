a048528a2515d2782a54b582693f167473cffc03
Rano-K <kmsoo0704@gmail.com>
2023-09-25 12:06:32 +0900

    위치서비스 test

---
+++ TravelMaker/Model/CurrentLocation.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentLocation.swift
+//  TravelMaker
+//
+//  Created by ms k on 2023/09/25.
+//
+
+import Foundation
---
+++ TravelMaker/ViewController/MapVC/test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  TravelMaker
+//
+//  Created by ms k on 2023/09/25.
+//
+
+import Foundation
